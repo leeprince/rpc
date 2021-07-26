fedd8dc0805651ffade41279e3420c9e1a405369
leeprince <leeprince@foxmail.com>
2021-07-27 01:27:24 +0800

    doc: 原生 rpc 调用注释

--- rpc/client/main.go
+++ rpc/client/main.go
@@ -25,6 +25,7 @@ func main() {
 	// Synchronous call: 同步调用
 	args := &Args{7, 8}
 	var reply int
+	// 调用 Arith 的 Multiply 方法
 	err = client.Call("Arith.Multiply", args, &reply)
 	if err != nil {
 		log.Fatal("arith error:", err)
@@ -33,6 +34,7 @@ func main() {
 	
 	// Asynchronous call: 异步调用
 	quotient := new(Quotient)
+	// 调用 Arith 的 Divide 方法
 	divCall := client.Go("Arith.Divide", args, quotient, nil)
 	replyCall := <-divCall.Done	// will be equal to divCall
 	// check errors, print, etc.
